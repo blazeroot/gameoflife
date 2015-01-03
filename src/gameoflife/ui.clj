be4ce2882af0ceb98908767c2125906840b6dc27
Tomasz Weissbek <blazeroot@gmail.com>
2015-01-03 15:53:53 +0100

    removed console UI, will use seesaw

--- src/gameoflife/ui.clj
+++ src/gameoflife/ui.clj
@@ -2,14 +2,4 @@
   (:require [clojure.core.matrix :as m]
             [seesaw.core :as saw]))
 
-(defn clear
-  "Clears the screen, using ANSI control characters."
-  []
-  (let [esc (char 27)]
-    (print (str esc "[2J"))
-    (print (str esc "[;H"))))
 
-(defn print-world
-  "Nicely pritns given matrix"
-  [matrix]
-  (m/pm matrix))
